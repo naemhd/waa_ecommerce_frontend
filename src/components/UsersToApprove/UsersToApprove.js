cc42b0fb8f6ed3e67744087cef185870fd7b5e11
Najed Hatamleh <najedhatamleh@Najeds-MacBook-Air.local>
2021-09-22 18:14:50 -0500

    Admin Page

---
+++ src/components/UsersToApprove/UsersToApprove.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const UsersToApprove = ({setUsersOrReviewFlag, usersOrReviewFlag}) => {
+
+    const [usersToApprove, setUsersToApprove] = useState([{ id: 1, name: "First User", billing_address: "Test" },
+    { id: 2, name: "Second User", billing_address: "One Two" }]);
+    const config = {
+        headers: { authorization: localStorage.getItem("userData") }
+    }
+
+    // useEffect(() => {
+    //     axios.get("/users-pending-approval", config).then((res) => {
+    //         setUsersToApprove(res.data);
+    //     })
+    // }, [])
+
+    const RenderUser = ({ user }) => {
+        return (
+        <section>
+            <h2>{user.name}</h2>
+            <h2>{user.billing_address}</h2>
+        </section>
+        )
+    }
+
+    return ( <div>
+        {usersToApprove.map(user => <RenderUser user={user} key={user.id}/>)}
+        <button onClick={() => setUsersOrReviewFlag(!usersOrReviewFlag)}> Go to Review</button>
+    </div>)
+}
+
+export default UsersToApprove;
--- src/shared/globals.js
+++ src/shared/globals.js
@@ -1,2 +1,7 @@
 export const BACKEND_SERVER = "http://localhost:8080";
 
+export const ADMIN_ROLE = "Admin";
+
+export const BUYER_ROLE = "Buyer";
+
+export const SELLER_ROLE = "Seller";
