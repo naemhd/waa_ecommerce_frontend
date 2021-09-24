0fd2a01b921e8067f18d6c42c0e5a6ae1a5a5c60
Emad Adel <emadadelabosaif@gmail.com>
2021-09-23 20:13:44 -0500

    fixes

--- src/pages/Orders/Orders.js
+++ src/pages/Orders/Orders.js
@@ -1,61 +1,58 @@
-import React, { useState, useEffect, useHistory } from "react";
+import React, { useState, useEffect } from "react";
 import { getAllOrderss } from "../../shared/api-calls/ordersAPI";
 import "./Orders.css";
 
 const Orders = (props) => {
   const [orders, setOrders] = useState([]);
   const userID = props.currentUser.id;
-
-  //const history = useHistory();
   useEffect(() => {
     getAllOrderss(userID)
       .then((data) => {
         setOrders(data);
-        console.log("orders ---- " + data);
       })
       .catch((e) => console.log(e));
-  }, []);
+  }, [userID]);
   return (
     <div className="products-container card-deck">
       {orders
         ? orders.map((o, index) => {
-            return (
-              <div key={index} className="card panel">
-                <div className="panel-heading">
-                  {"  "}
-                  <label>Order # {o.id}</label>&nbsp; &nbsp; &nbsp; &nbsp;
-                  <label>
-                    {" "}
-                    Total:{" "}
-                    {o.product
-                      .map((p) => p.price)
-                      .reduce((sum, current) => sum + current, 0)}{" "}
-                    $
-                  </label>
-                  &nbsp; &nbsp; &nbsp; &nbsp;
-                  <label>Status : {o.status}</label>
-                </div>
-                {o.product.map((p, index) => {
-                  return (
-                    <div
-                      key={index}
-                      className="card"
-                      style={{ margin: "15px" }}
-                    >
-                      <div className="card-body">
-                        <h5 className="card-title">{p.title}</h5>
-                        <h6 className="card-subtitle mb-2 text-muted">
-                          {p.price}
-                        </h6>
-                        <p className="card-text">{p.description}</p>
-                        {/* <button className="btn btn-link">Add to cart</button> */}
-                      </div>
-                    </div>
-                  );
-                })}
+          return (
+            <div key={index} className="card panel">
+              <div className="panel-heading">
+                {"  "}
+                <label>Order # {o.id}</label>&nbsp; &nbsp; &nbsp; &nbsp;
+                <label>
+                  {" "}
+                  Total:{" "}
+                  {o.product
+                    .map((p) => p.price)
+                    .reduce((sum, current) => sum + current, 0)}{" "}
+                  $
+                </label>
+                &nbsp; &nbsp; &nbsp; &nbsp;
+                <label>Status : {o.status}</label>
               </div>
-            );
-          })
+              {o.product.map((p, index) => {
+                return (
+                  <div
+                    key={index}
+                    className="card"
+                    style={{ margin: "15px" }}
+                  >
+                    <div className="card-body">
+                      <h5 className="card-title">{p.title}</h5>
+                      <h6 className="card-subtitle mb-2 text-muted">
+                        {p.price}
+                      </h6>
+                      <p className="card-text">{p.description}</p>
+                      {/* <button className="btn btn-link">Add to cart</button> */}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })
         : null}
     </div>
   );
--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,15 +1,29 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { getAllProducts } from "../../shared/api-calls/productsAPI";
+import { deleteProduct, getAllProducts, getUserProducts } from "../../shared/api-calls/productsAPI";
+import { SELLER_ROLE } from "../../shared/globals";
 import "./Products.css"
 
-const Products = ({ products, setProducts, cartAddingHandler, cartProducts }) => {
+const Products = ({ products, setProducts, cartAddingHandler, cartProducts, currentUser }) => {
 
     useEffect(() => {
-        getAllProducts().then((data) => {
-            setProducts(data);
+        if (currentUser.role === SELLER_ROLE) {
+            getUserProducts().then((data) => {
+                setProducts(data);
+            }).catch(e => console.log(e));
+        } else {
+            getAllProducts().then((data) => {
+                setProducts(data);
+            }).catch(e => console.log(e))
+        }
+    }, [currentUser.role, setProducts]);
+
+    const onDelete = (productId) => {
+        deleteProduct(productId).then(res => {
+            const updatedProducts = products.filter(p => p.id !== productId);
+            setProducts(updatedProducts);
         }).catch(e => console.log(e))
-    }, []);
+    }
     return (
         <div className="products-container card-deck">
             {products ? products.map((p, index) => {
@@ -19,16 +33,24 @@ const Products = ({ products, setProducts, cartAddingHandler, cartProducts }) =>
                             <h5 className="card-title">{p.title}</h5>
                             <h6 className="card-subtitle mb-2 text-muted">{p.price}</h6>
                             <p className="card-text">{p.description}</p>
-                            {cartProducts.find(cp => cp.id === p.id) ?
-                                <label className="label label-success">Added</label>
+                            {currentUser.role === SELLER_ROLE ?
+                                <button onClick={() => onDelete(p.id)} className="btn btn-danger">Delete</button>
                                 :
-                                <button onClick={() => cartAddingHandler(p)} className="btn btn-link">Add to cart</button>
+                                <>
+                                    {cartProducts.find(cp => cp.id === p.id) ?
+                                        <label className="label label-success">Added</label>
+                                        :
+                                        <button onClick={() => cartAddingHandler(p)} className="btn btn-link">Add to cart</button>
+                                    }
+                                    <Link
+                                        to={`/product-details/${p.id}`}
+                                        className="btn btn-link">
+                                        Details
+                                    </Link>
+                                </>
                             }
-                            <Link
-                                to={`/product-details/${p.id}`}
-                                className="btn btn-link">
-                                Details
-                            </Link>
+
+
                         </div>
                     </div>
                 )
